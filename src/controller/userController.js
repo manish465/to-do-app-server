6338e6868c58c67f1395e3c85ff3923c783e8475
manish <ms5978307@gmail.com>
2023-04-03 20:33:05 +0530

    added user login feature

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 exports.addUser = (req, res) => {
     const { firstName, lastName, email, password } = req.body;
@@ -7,19 +8,38 @@ exports.addUser = (req, res) => {
     if (!firstName || !lastName || !email || !password)
         return res.status(400).json({ error: "Enter all required field" });
 
-    User.findOne({ email }).then((user) => {
-        if (user) return res.status(400).json({ error: "User already exist" });
+    User.findOne({ email }).then((u) => {
+        if (u) return res.status(400).json({ error: "User already exist" });
 
         const hashPassword = bcrypt.hashSync(password, 3);
-        const _user = User({
+        const user = new User({
             firstName,
             lastName,
             email,
             password: hashPassword,
         });
-        _user
-            .save()
+        user.save()
             .then(() => res.status(200).json({ message: "User added" }))
             .catch((error) => res.status(400).json({ error: error.message }));
+    });
+};
+
+exports.signin = (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+        return res.status(400).json({ error: "Enter all required field" });
+
+    User.findOne({ email }).then((u) => {
+        if (!u) return res.status(400).json({ error: "User dose not exist" });
+
+        bcrypt.compare(password, u.password).then((result) => {
+            if (result) {
+                const token = jwt.sign({ id: u._id }, process.env.JWT_SECRET);
+                return res
+                    .status(200)
+                    .json({ token, message: "Login sucsses" });
+            } else return res.status(400).json({ error: "Invalid Inputs" });
+        });
     });
 };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { addUser } = require("../controller/userController");
+const { addUser, signin } = require("../controller/userController");
 
 router.post("/add-user", addUser);
+router.post("/sign-in", signin);
 
 module.exports = router;
